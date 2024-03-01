deb3e218a8a04bdfff4a18117c74e0e3c567d710
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-01 22:20:19 +0000

    fix: home-manager is bugging out

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-colors.url = "github:misterio77/nix-colors";
