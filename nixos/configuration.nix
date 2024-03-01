3d03792fcf1892133aa1762dff04b2435b739bcd
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-01 21:53:15 +0000

    fix: correct home manager version

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,7 +81,7 @@
       openssh.authorizedKeys.keys = [
 	      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUTnMjW7jO8v4nyw5XtEioOz7IuthBODYYA8V44u/12 dominicfmazza"
       ];
-      extraGroups = ["wheel", "docker"];
+      extraGroups = ["wheel" "docker"];
     };
   };
 
