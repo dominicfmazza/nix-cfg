7107fb70e79141e9863eb98705507a1929e76819
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-02 01:33:13 +0000

    fix: nvim config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,13 +11,11 @@
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
+    outputs.homeManagerModules.nvim
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
-    # You can also split up your configuration and import pieces of it here:
-    ./nvim/default.nix
   ];
 
   nixpkgs = {
