402f26af7fd4145c05cf271522fb2f6ac6f6900a
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-05 23:29:08 -0700

    feat: telescope keybinds now handled by which key

--- modules/home-manager/nvim/config/lua/plugins/telescope.lua
+++ modules/home-manager/nvim/config/lua/plugins/telescope.lua
@@ -1,8 +1,16 @@
 -- Telescope Settings
-local opts = { noremap = true }
-require("legendary").keymaps({
-	{ "<Leader>ff", require("telescope.builtin").find_files, description = "Telescope: Find files", opts = opts },
-	{ "<Leader>fg", require("telescope.builtin").live_grep, description = "Telescope: Live grep", opts = opts },
-	{ "<Leader>fb", require("telescope.builtin").buffers, description = "Telescope: Buffers", opts = opts },
-	{ "<Leader>fh", require("telescope.builtin").help_tags, description = "Telescope: Help tags", opts = opts },
-})
+local wk = require("which-key")
+wk.register({
+        f = {
+            name = "+telescope",
+            f = { require("telescope.builtin").find_files, "Find Files" },
+            g = { require("telescope.builtin").live_grep, "Live Grep" },
+            b = { require("telescope.builtin").buffers, "Buffers" },
+            h = { require("telescope.builtin").help_tags, "Help Tags" },
+        }
+    },
+    {
+        noremap = true,
+        prefix = "<leader>"
+    }
+)
