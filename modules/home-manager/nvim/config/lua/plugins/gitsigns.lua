a217804382f842676beaeb04aa9d965292772e3b
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-05 21:53:39 -0700

    fix: git keybinds

--- modules/home-manager/nvim/config/lua/plugins/gitsigns.lua
+++ modules/home-manager/nvim/config/lua/plugins/gitsigns.lua
@@ -13,15 +13,15 @@ local wk = require("which-key")
 wk.register({
         g = {
             name = "+git",
-            l = { function() require("gitsigns").blame_line() end, desc = "View Git blame" },
-            L = { function() require("gitsigns").blame_line { full = true } end, desc = "View full Git blame" },
-            p = { function() require("gitsigns").preview_hunk() end, desc = "Preview Git hunk" },
-            h = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" },
-            r = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" },
-            s = { function() require("gitsigns").stage_hunk() end, desc = "Stage Git hunk" },
-            S = { function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" },
-            u = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" },
-            d = { function() require("gitsigns").diffthis() end, desc = "View Git diff" },
+            l = { function() require("gitsigns").blame_line() end, "Blame" },
+            L = { function() require("gitsigns").blame_line { full = true } end, "Full Blame" },
+            p = { function() require("gitsigns").preview_hunk() end, "Preview Hunk" },
+            h = { function() require("gitsigns").reset_hunk() end, "Reset Hunk" },
+            r = { function() require("gitsigns").reset_buffer() end, "Reset Buffer" },
+            s = { function() require("gitsigns").stage_hunk() end, "Stage Hunk" },
+            S = { function() require("gitsigns").stage_buffer() end, "Stage Buffer" },
+            u = { function() require("gitsigns").undo_stage_hunk() end, "Unstage Hunk" },
+            d = { function() require("gitsigns").diffthis() end, "View Diff" },
         }
     },
     { prefix = "<leader>" }
