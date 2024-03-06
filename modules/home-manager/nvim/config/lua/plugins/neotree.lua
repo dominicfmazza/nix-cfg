c81b433508412bcb99bfea2ce20fcdd24eac130c
Dominic Mazza <dominicfmazza@gmail.com>
2024-03-05 19:21:37 -0700

    feat: neotree keybinds

--- modules/home-manager/nvim/config/lua/plugins/neotree.lua
+++ modules/home-manager/nvim/config/lua/plugins/neotree.lua
@@ -1,1 +1,8 @@
 require("neo-tree").setup({})
+
+local wk = require("which-key")
+
+wk.register({
+    e = {"<cmd>Neotree toggle<cr>", "Neotree: Toggle"},
+    o = {"<cmd>Neotree focus<cr>", "Neotree: Focus"},
+}, {prefix="<leader>"})
